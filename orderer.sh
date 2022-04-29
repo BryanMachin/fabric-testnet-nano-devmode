d12ebdf96f2d252013987ef6c275b7e378064a28
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-04-29 12:03:02 -0400

    added genesis validation

--- orderer.sh
+++ orderer.sh
@@ -4,9 +4,15 @@ set -eu
 # imports
 . ./env.sh
 
+export ORDERER_GENERAL_BOOTSTRAPFILE="${PWD}"/channel-artifacts/genesis.block
+
+if [ ! -f "${ORDERER_GENERAL_BOOTSTRAPFILE}" ]; then
+	echo "==> There is no genesis block, make sure you run the generate-artifacts script first."
+	return 1
+fi
+
 export FABRIC_LOGGING_SPEC=debug:cauthdsl,policies,msp,common.configtx,common.channelconfig=info
 export ORDERER_GENERAL_TLS_ENABLED=false
-export ORDERER_GENERAL_BOOTSTRAPFILE="${PWD}"/channel-artifacts/genesis.block
 export ORDERER_FILELEDGER_LOCATION="${PWD}"/data/orderer
 export ORDERER_GENERAL_LOCALMSPDIR="${PWD}"/crypto-config/msp
 
