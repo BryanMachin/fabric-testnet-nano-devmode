03725a80f01052ea6ddca3667d6edd7244988882
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-05-05 17:17:01 -0400

    fix several issues

--- env.sh
+++ env.sh
@@ -9,3 +9,5 @@ export FABRIC_CFG_PATH="${PWD}"/../config
 export FABRIC_LOGGING_SPEC=INFO
 export CORE_PEER_TLS_ENABLED=false
 export CORE_PEER_MSPCONFIGPATH="${PWD}"/crypto-config/msp
+export CORE_PEER_ADDRESS=127.0.0.1:7051
+export CORE_PEER_LOCALMSPID=SampleOrg
--- peer.sh
+++ peer.sh
@@ -5,7 +5,12 @@ set -eu
 . ./env.sh
 
 export FABRIC_LOGGING_SPEC=chaincode=debug
-export CORE_PEER_CHAINCODELISTENADDRESS=127.0.0.1:7052
+export CORE_PEER_ID=peer0.org1.example.com
+export CORE_PEER_LISTENADDRESS=0.0.0.0:7051
+export CORE_PEER_CHAINCODEADDRESS=127.0.0.1:7052
+export CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:7052
+export CORE_PEER_GOSSIP_EXTERNALENDPOINT=127.0.0.1:7051
+export CORE_PEER_GOSSIP_BOOTSTRAP=127.0.0.1:7051
 export CORE_OPERATIONS_LISTENADDRESS=127.0.0.1:8446
 export CORE_PEER_FILESYSTEMPATH="${PWD}"/data/peer0.org1.example.com
 export CORE_LEDGER_SNAPSHOTS_ROOTDIR="${PWD}"/data/peer0.org1.example.com/snapshots
@@ -13,9 +18,16 @@ export CORE_LEDGER_SNAPSHOTS_ROOTDIR="${PWD}"/data/peer0.org1.example.com/snapsh
 # uncomment the lines below to utilize couchdb state database, when done with the environment you can stop the couchdb container with "docker rm -f worldstate"
 # export CORE_LEDGER_STATE_STATEDATABASE=CouchDB
 # export CORE_LEDGER_STATE_COUCHDBCONFIG_COUCHDBADDRESS=127.0.0.1:5984
-# export CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME=admin
-# export CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD=adminpw
-# docker run --publish 5984:5984 --detach -e COUCHDB_USER=admin -e COUCHDB_PASSWORD=adminpw --name worldstate couchdb:3.1.1
+# export CORE_LEDGER_STATE_COUCHDBCONFIG_USERNAME=portainer
+# export CORE_LEDGER_STATE_COUCHDBCONFIG_PASSWORD=N@rut0
+# # remove the container only if it exists
+# CONTAINER_NAME=$(docker ps -aqf "NAME=worldstate")
+# if [ -z "$CONTAINER_NAME" -o "$CONTAINER_NAME" == " " ]; then
+# 	echo "---- No world-state container available for deletion ----"
+# else
+# 	docker rm -f $CONTAINER_NAME
+# fi
+# docker run --publish 5984:5984 --detach -e COUCHDB_USER=portainer -e COUCHDB_PASSWORD=N@rut0 --name worldstate couchdb:3.1.1
 
 # start peer node in development mode
 peer node start --peer-chaincodedev=true
