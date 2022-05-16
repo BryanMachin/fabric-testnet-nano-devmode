e964a5ae704308b678dbe0225fad7bf19d755b3e
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-05-16 15:35:54 -0400

    fix

--- peer.sh
+++ peer.sh
@@ -7,9 +7,9 @@ set -eu
 export FABRIC_LOGGING_SPEC=chaincode=debug
 export CORE_PEER_LOCALMSPID=Org1MSP
 export CORE_PEER_ID=peer0.org1.example.com
-export CORE_PEER_LISTENADDRESS=0.0.0.0:7051
+export CORE_PEER_LISTENADDRESS=127.0.0.1:7051
 export CORE_PEER_CHAINCODEADDRESS=127.0.0.1:7052
-export CORE_PEER_CHAINCODELISTENADDRESS=0.0.0.0:7052
+export CORE_PEER_CHAINCODELISTENADDRESS=127.0.0.1:7052
 # gossip
 export CORE_PEER_GOSSIP_EXTERNALENDPOINT=127.0.0.1:7051
 export CORE_PEER_GOSSIP_BOOTSTRAP=127.0.0.1:7051
@@ -17,7 +17,7 @@ export CORE_PEER_FILESYSTEMPATH="${PWD}"/data/peer0.org1.example.com
 export CORE_LEDGER_SNAPSHOTS_ROOTDIR="${PWD}"/data/peer0.org1.example.com/snapshots
 # used in metrics
 export CORE_METRICS_PROVIDER=prometheus
-export CORE_OPERATIONS_LISTENADDRESS=0.0.0.0:8446
+export CORE_OPERATIONS_LISTENADDRESS=127.0.0.1:8446
 
 
 # uncomment the lines below to utilize couchdb state database, when done with the environment you can stop the couchdb container with "docker rm -f worldstate"
