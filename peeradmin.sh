9e87b70e89b1db145a3ac768e3b26c2b4e5e4997
Kmilo Denis Glez <kmilo.denis.glez@gmail.com>
2022-05-16 10:52:31 -0400

    modify peeradmin.sh

--- peeradmin.sh
+++ peeradmin.sh
@@ -3,6 +3,9 @@
 # imports
 . ./env.sh
 
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_MSPCONFIGPATH="${PWD}"/crypto-config/organizations/org1.example.com/users/Admin@org1.example.com/msp
+
 peer channel create -o 127.0.0.1:7050 -c mychannel -f "${PWD}"/channel-artifacts/mychannel.tx --outputBlock "${PWD}"/channel-artifacts/mychannel.block
 
 # Now join the peer to the channel 
