cb54710b34a0858edf86e88487f344f0f4f2511b
Linux User <portainer@localhost.localdomain>
2022-05-16 16:33:24 +0000

    chmod +x and fix issue

--- dapp_env.sh
+++ dapp_env.sh
@@ -13,5 +13,5 @@ case "$(uname -s)" in
 esac
 
 # Set environment variables for the dapp
-export HLF_DAPP_CONFIG="${PWD}/${OS_FILECONFIG}"
+export HLF_DAPP_CONFIG="${PWD}/dapp/${OS_FILECONFIG}"
 export HLF_DAPP_JWT_SIGN_KEY="45567f001601aacb761e13987cddc62ddd49c5b2"
