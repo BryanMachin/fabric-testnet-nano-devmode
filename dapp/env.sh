e222d26ff61c40645f1ae6a7e37a5079478fd2c6
Linux User <portainer@localhost.localdomain>
2022-05-16 16:23:26 +0000

    added dapp folder and env.sh

---
+++ dapp/env.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+
+case "$(uname -s)" in
+   CYGWIN*|MINGW32*|MSYS*|MINGW*)
+	 OS_FILECONFIG=conf.window.yaml
+     ;;
+
+   *)
+	# Linux or Darwin 
+     OS_FILECONFIG=conf.linux.yaml
+     ;;
+esac
+
+# Set environment variables for the dapp
+export HLF_DAPP_CONFIG="${PWD}/${OS_FILECONFIG}"
+export HLF_DAPP_JWT_SIGN_KEY="45567f001601aacb761e13987cddc62ddd49c5b2"
